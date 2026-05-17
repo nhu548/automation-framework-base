9f921491f2b8fd5c3f11e8607f4f38d9e5bfea99
Hoai Nhu <nguyenthihoainhu07@gmail.com>
2026-05-18 01:55:20 +0700

    Implement BasePage reusable methods

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -2,22 +2,30 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class BasePage {
 
     WebDriver driver;
+    WebDriverWait wait;
 
     public BasePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     // Common Methods
 
     public void enterText(By locator, String text) {
+        waitForElementVisible(locator);
         driver.findElement(locator).sendKeys(text);
     }
 
     public void clickElement(By locator) {
+        waitForElementVisible(locator);
         driver.findElement(locator).click();
     }
 
@@ -27,5 +35,9 @@ public class BasePage {
 
     public String getCurrentUrl() {
         return driver.getCurrentUrl();
+    }
+
+    public void waitForElementVisible(By locator) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
     }
 }
